63e0acb9b70247548f0e70e38002e1dfeb4cbad7
ppolanco <paropoga@gmail.com>
2016-12-28 09:30:28 -0300

    Adding views

---
+++ app/src/main/java/blopa/beacons/Setter.java
@@ -0,0 +1,54 @@
+package blopa.beacons;
+
+/**
+ * Created by Blopa on 26-12-2016.
+ */
+
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.Beacon;
+//import com.skybeacon.sdk.*;
+
+//import com.estimote.sdk.connection.BeaconConnection
+//
+//public class Setter {
+//
+//    private void setMajorID(final int majorid,final Beacon beacon) {
+//
+//        mMajorsConnection = new BeaconConnection(this, beacon, new BeaconConnection.ConnectionCallback() {
+//            @Override
+//            public void onAuthenticated(BeaconConnection.BeaconCharacteristics chars) {
+//                Log.d(TAG, "Authenticated to beacon: " + chars);
+//                mMajorsConnection.writeMajor(majorid, new BeaconConnection.WriteCallback() {
+//                    @Override
+//                    public void onSuccess() {
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                mAdapter.update(beacon);
+//                            }
+//                        });
+//                        Log.d(TAG, "Successfully writted the major id!");
+//                        mMajorsConnection.close();
+//                    }
+//
+//                    @Override
+//                    public void onError() {
+//                        Log.d(TAG, "Error while writting the major id!");
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onAuthenticationError() {
+//                Log.d(TAG, "Authentication Error");
+//            }
+//
+//            @Override
+//            public void onDisconnected() {
+//                Log.d(TAG, "Disconnected");
+//            }
+//        });
+//        mMajorsConnection.authenticate();
+//    }
+//}
